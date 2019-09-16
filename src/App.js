240fe2a02c334e843fd1be225cce93a6534831ad
Oliver Schneider <os@ols.io>
2019-09-16 12:56:01 +0200

    better

--- src/App.js
+++ src/App.js
@@ -1,32 +1,65 @@
 import React from 'react'
 import './App.css'
-import Balloon from './Balloon'
+import logo from './balloon.png'
+import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
-import Container from '@material-ui/core/Container'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 import { CSSTransitionGroup } from 'react-transition-group'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh'
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
+
 const App = () => {
+  const classes = useStyles()
   const [scale, setScale] = React.useState(40)
   const [exploded, setExploded] = React.useState(false)
 
-  const balloon = exploded ? null : <Balloon key={scale} size={scale} />
+  const balloon = exploded ? null : (
+    <img src={logo} key={scale} alt="logo" style={{ height: `${scale}vmin` }} />
+  )
 
   return (
-    <div className="App">
-      <Container maxWidth="xl">
-        <Grid container direction="column" justify="center" alignItems="center">
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
           <TextField
             id="outlined-name"
             label="Scale"
             margin="normal"
             variant="outlined"
             disabled="true"
+            fullWidth
             value={scale}
           />
           <Button
             variant="contained"
+            fullWidth
             color="primary"
             onClick={() => {
               const newScale = scale + 5
@@ -39,17 +72,19 @@ const App = () => {
           >
             Hello World
           </Button>
-        </Grid>
-        <Grid container direction="row" justify="center" alignItems="center">
+        </div>
+      </Grid>
+      <Grid item xs={12} sm={4} md={7} component={Paper} square>
+        <div className={classes.paper}>
           <CSSTransitionGroup
             transitionName="example"
             transitionLeave={exploded}
           >
             {balloon}
           </CSSTransitionGroup>
-        </Grid>
-      </Container>
-    </div>
+        </div>
+      </Grid>
+    </Grid>
   )
 }
 export default App
